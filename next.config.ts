10b5580647c13b3d6014d3ea956e36ca680d9042
Muhammad Rivky Ananda <rivkyananda33@gmail.com>
2025-12-26 13:19:17 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
